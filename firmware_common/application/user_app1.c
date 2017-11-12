a7181e606ac23fb45584c493406680c43be6cc57
weisihang <sihang_wei@outlook.com>
2017-11-13 00:12:45 +0800

    new

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -51,6 +51,8 @@ extern volatile u32 G_u32ApplicationFlags;             /* From main.c */
 
 extern volatile u32 G_u32SystemTime1ms;                /* From board-specific source file */
 extern volatile u32 G_u32SystemTime1s;                 /* From board-specific source file */
+extern u8 G_au8DebugScanfBuffer[];  /* From debug.c */
+extern u8 G_u8DebugScanfCharCount;  /* From debug.c */
 
 
 /***********************************************************************************************************************
@@ -66,7 +68,10 @@ static u8 UserApp_au8LCD3[] = "B1:DEBUG";
 static u8 UserApp_au8LCD4[] = "B3:MENU";
 static u8 UserApp_au8LCD5[] = "B0:LEFT";
 static u8 UserApp_au8LCD6[] = "B1:RIGHT";
+static u8 UserApp_au8LCD7[] = "INPUT MESSAGES";
+static u8 UserApp_au8LCD8[] = "PRESS\"ENTER\"";
 static u8 UserApp_CursorPosition;
+static u8 UserApp_au8UserInputBuffer[U16_USER_INPUT_BUFFER_SIZE];  
 
 /**********************************************************************************************************************
 Function Definitions
@@ -100,6 +105,19 @@ void UserApp1Initialize(void)
   LCDMessage(LINE2_START_ADDR,UserApp_au8LCD2); 
   LCDMessage(LINE2_START_ADDR+12,UserApp_au8LCD3);  
   UserApp_CursorPosition = LINE1_START_ADDR;
+  LedOff(WHITE);
+  LedOff(PURPLE);
+  LedOff(BLUE);
+  LedOff(CYAN);
+  LedOff(GREEN);
+  LedOff(YELLOW);
+  LedOff(ORANGE);
+  LedOff(RED);
+  for(u16 i = 0; i < U16_USER_INPUT_BUFFER_SIZE  ; i++)
+  {
+    UserApp_au8UserInputBuffer[i] = 0;
+  }
+
 
  
   /* If good initialization, set state to Idle */
@@ -154,8 +172,11 @@ static void UserApp1SM_Idle(void)
   static bool bMoveLeft = FALSE;
   static bool bMoveRight = FALSE;
   static bool bexcuationOfMove = FALSE;
-
-  if(!bexcuationOfMove&&!bMoveLeft&&!bMoveRight)
+  static bool bDebug = FALSE;
+  static u8 u8ArrayCount = 0;
+  static u8 u8CharCount = 0;
+  
+  if(!bexcuationOfMove&&!bMoveLeft&&!bMoveRight&&!bDebug)
   {
     if(WasButtonPressed(BUTTON0))
     {
@@ -166,37 +187,131 @@ static void UserApp1SM_Idle(void)
       LCDMessage(LINE2_START_ADDR+8, UserApp_au8LCD6);
       bexcuationOfMove = TRUE;
     }
+    if(WasButtonPressed(BUTTON1))
+    {
+      ButtonAcknowledge(BUTTON1);
+      LCDCommand(LCD_CLEAR_CMD);
+      LCDMessage(LINE1_START_ADDR, UserApp_au8LCD7);
+      LCDMessage(LINE2_START_ADDR, UserApp_au8LCD8);
+      LCDMessage(LINE2_START_ADDR+13, UserApp_au8LCD4);
+      bDebug = TRUE;
+    /*  if(G_au8DebugScanfBuffer[u8ArrayCount] != '\n')
+      {
+        UserApp_au8UserInputBuffer[u8ArrayCount] = G_au8DebugScanfBuffer[u8ArrayCount];
+        u8ArrayCount++;
+      }
+      u8ArrayCount=0;
+      u8CharCount = DebugScanf(UserApp_au8UserInputBuffer);
+      UserApp_au8UserInputBuffer[u8CharCount] = '\0';
+      if(u8CharCount<=20)
+      {
+        LCDCommand(LCD_CLEAR_CMD);
+        LCDMessage/(LINE1_START_ADDR, UserApp_au8UserInputBuffer);
+      }*/
+    }
   }
-  if(bexcuationOfMove&&!bMoveLeft&&!bMoveRight)
+  if(bexcuationOfMove&&!bMoveLeft&&!bMoveRight&&!bDebug)
   {
     if(WasButtonPressed(BUTTON0))
     {
       ButtonAcknowledge(BUTTON0);
       bMoveLeft = TRUE;
     }
+    if(WasButtonPressed(BUTTON1))
+    {
+      ButtonAcknowledge(BUTTON1);
+      bMoveRight = TRUE;
+    }
   }
-  if(bMoveLeft)
+  if(bMoveRight)
   {
+    PWMAudioOn(BUZZER1);
     if(u16MovementCounter==1000)
     {
-      if(UserApp_CursorPosition==LINE1_END_ADDR)
+      if(UserApp_CursorPosition>LINE1_END_ADDR)
       {
         UserApp_CursorPosition=LINE1_START_ADDR;
       }
       LCDCommand(LCD_CLEAR_CMD);
-      UserApp_CursorPosition++;
       LCDMessage(LINE1_START_ADDR+UserApp_CursorPosition, UserApp_au8MyName);
       LCDMessage(LINE2_START_ADDR, UserApp_au8LCD4);
-      LCDMessage(LINE2_START_ADDR+8, UserApp_au8LCD6);
+      LCDMessage(LINE2_START_ADDR+8, UserApp_au8LCD5);
       u16MovementCounter=0;
+      UserApp_CursorPosition++;
     }
     else
     {
       u16MovementCounter++;
     } 
+    if(WasButtonPressed(BUTTON0))
+    {
+      LedOff(WHITE);
+      LedOff(PURPLE);
+      LedOff(BLUE);
+      LedOff(CYAN);
+      LedOff(GREEN);
+      LedOff(YELLOW);
+      LedOff(ORANGE);
+      LedOff(RED);
+      ButtonAcknowledge(BUTTON0);
+      bMoveRight = FALSE;
+      bMoveLeft = TRUE;
+      u16MovementCounter = 0;
+    }
+    if((UserApp_CursorPosition==LINE1_START_ADDR)||(UserApp_CursorPosition>LINE1_END_ADDR))
+    {
+      PWMAudioSetFrequency(BUZZER1, 1000);
+      LedOn(WHITE);
+      LedOff(RED);
+    }
+    if(UserApp_CursorPosition==2&&u16MovementCounter==500)
+    {
+      PWMAudioSetFrequency(BUZZER1, 875);
+      LedOn(PURPLE);
+      LedOff(WHITE);
+    }
+    if(UserApp_CursorPosition==5)
+    {
+      PWMAudioSetFrequency(BUZZER1, 750);
+      LedOn(BLUE);
+      LedOff(PURPLE);
+    }
+    if(UserApp_CursorPosition==7&&u16MovementCounter==500)
+    {
+      PWMAudioSetFrequency(BUZZER1, 625);
+      LedOn(CYAN);
+      LedOff(BLUE);
+    }
+    if(UserApp_CursorPosition==10)
+    {
+      PWMAudioSetFrequency(BUZZER1, 500);
+      LedOn(GREEN);
+      LedOff(CYAN);
+    }
+    if(UserApp_CursorPosition==12&&u16MovementCounter==500)
+    {
+      PWMAudioSetFrequency(BUZZER1, 375);
+      LedOn(YELLOW);
+      LedOff(GREEN);
+    }
+    if(UserApp_CursorPosition==15)
+    {
+      PWMAudioSetFrequency(BUZZER1, 250);
+      LedOn(ORANGE);
+      LedOff(YELLOW);
+    }
+    if(UserApp_CursorPosition==17&&u16MovementCounter==500)
+    {
+      PWMAudioSetFrequency(BUZZER1, 125);
+      LedOn(RED);
+      LedOff(ORANGE);
+    } 
   }
-  if(bMoveRight)
+  
+  if(bMoveLeft)
   {
+    PWMAudioOn(BUZZER1);
+    PWMAudioSetFrequency(BUZZER1, 500);
     if(u16MovementCounter==1000)
     {
       if(UserApp_CursorPosition==LINE1_START_ADDR)
@@ -204,18 +319,119 @@ static void UserApp1SM_Idle(void)
         UserApp_CursorPosition=LINE1_END_ADDR;
       }
       LCDCommand(LCD_CLEAR_CMD);
-      UserApp_CursorPosition++;
-      LCDMessage(LINE1_START_ADDR-UserApp_CursorPosition, UserApp_au8MyName);
+      LCDMessage(UserApp_CursorPosition, UserApp_au8MyName);
       LCDMessage(LINE2_START_ADDR, UserApp_au8LCD4);
       LCDMessage(LINE2_START_ADDR+8, UserApp_au8LCD6);
       u16MovementCounter=0;
+      UserApp_CursorPosition--;
     }
     else
     {
       u16MovementCounter++;
     } 
+    if(WasButtonPressed(BUTTON1))
+    {
+      LedOff(WHITE);
+      LedOff(PURPLE);
+      LedOff(BLUE);
+      LedOff(CYAN);
+      LedOff(GREEN);
+      LedOff(YELLOW);
+      LedOff(ORANGE);
+      LedOff(RED);
+      ButtonAcknowledge(BUTTON1);
+      bMoveLeft = FALSE;
+      bMoveRight = TRUE;
+      u16MovementCounter = 0;
+    }
+    if(UserApp_CursorPosition==LINE1_START_ADDR)
+    {
+      PWMAudioSetFrequency(BUZZER1, 125);
+      LedOn(RED);
+      LedOff(WHITE);
+    }
+    if(UserApp_CursorPosition==17&&u16MovementCounter==500)
+    {
+      PWMAudioSetFrequency(BUZZER1, 250);
+      LedOn(ORANGE);
+      LedOff(RED);
+    }
+    if(UserApp_CursorPosition==15)
+    {
+      PWMAudioSetFrequency(BUZZER1, 375);
+      LedOn(YELLOW);
+      LedOff(ORANGE);
+    }
+    if(UserApp_CursorPosition==12&&u16MovementCounter==500)
+    {
+      PWMAudioSetFrequency(BUZZER1, 500);
+      LedOn(GREEN);
+      LedOff(YELLOW);
+    }
+    if(UserApp_CursorPosition==10)
+    {
+      PWMAudioSetFrequency(BUZZER1, 625);
+      LedOn(CYAN);
+      LedOff(GREEN);
+    }
+    if(UserApp_CursorPosition==7&&u16MovementCounter==500)
+    {
+      PWMAudioSetFrequency(BUZZER1, 750);
+      LedOn(BLUE);
+      LedOff(CYAN);
+    }
+    if(UserApp_CursorPosition==5)
+    {
+      PWMAudioSetFrequency(BUZZER1, 875);
+      LedOn(PURPLE);
+      LedOff(BLUE);
+    }
+    if(UserApp_CursorPosition==2&&u16MovementCounter==500)
+    {
+      PWMAudioSetFrequency(BUZZER1, 1000);
+      LedOn(WHITE);
+      LedOff(PURPLE);
+    } 
   }
- 
+  if(WasButtonPressed(BUTTON3))
+  {
+    ButtonAcknowledge(BUTTON3);
+    LCDCommand(LCD_CLEAR_CMD);
+    LCDMessage(LINE1_START_ADDR, UserApp_au8LCD1);
+    LCDMessage(LINE1_START_ADDR+13,UserApp_au8LCD4);
+    LCDMessage(LINE2_START_ADDR,UserApp_au8LCD2); 
+    LCDMessage(LINE2_START_ADDR+12,UserApp_au8LCD3);  
+    UserApp_CursorPosition = LINE1_START_ADDR;
+    bMoveLeft = FALSE;
+    bMoveRight = FALSE;
+    bexcuationOfMove = FALSE;
+    LedOff(WHITE);
+    LedOff(PURPLE);
+    LedOff(BLUE);
+    LedOff(CYAN);
+    LedOff(GREEN);
+    LedOff(YELLOW);
+    LedOff(ORANGE);
+    LedOff(RED); 
+    PWMAudioOff(BUZZER1);
+    bDebug = FALSE;
+  }
+  if(bDebug)
+  {
+    while(G_au8DebugScanfBuffer[u8ArrayCount] != '\n')
+    {
+      UserApp_au8UserInputBuffer[u8ArrayCount] = G_au8DebugScanfBuffer[u8ArrayCount];
+      u8ArrayCount++;
+    }
+    u8ArrayCount=0;
+    u8CharCount = DebugScanf(UserApp_au8UserInputBuffer);
+    UserApp_au8UserInputBuffer[u8CharCount] = '\0';
+    if(u8CharCount<=20)
+    {
+      LCDCommand(LCD_CLEAR_CMD);
+      LCDMessage(LINE1_START_ADDR, UserApp_au8UserInputBuffer);
+    }
+  }
 } /* end UserApp1SM_Idle() */
     
 
